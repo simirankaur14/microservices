1ae29415455935415244de317f9f75d8a8d1a0b2
simirankaur14 <55805745+simirankaur14@users.noreply.github.com>
2025-01-18 14:14:27 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -92,7 +92,7 @@ resource "aws_lambda_function" "test_lambda" {
   filename      = "./lambda_function_folder.zip"
   function_name = "test_name"
   role          = "arn:aws:iam::816069139413:role/lambdarole"
-  handler       = "lambda_function.lambda_function.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   runtime = "python3.9"
 
   environment {
@@ -157,6 +157,57 @@ resource "aws_security_group" "ansible_master_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
+}
+
+# Stepfunction Block
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name     = "my-state-machine"
+  role_arn = "arn:aws:iam::816069139413:role/stepfunction_lambdaaccess_role"
+
+  definition = file("${path.module}/defination.yaml")
+}
+
+# APIGATEWAY Block
+
+resource "aws_api_gateway_rest_api" "MyDemoAPI" {
+  name        = "MyDemoAPI"
+  description = "This is my API for demonstration purposes"
+}
+
+resource "aws_api_gateway_resource" "MyDemoResource" {
+  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+  parent_id   = aws_api_gateway_rest_api.MyDemoAPI.root_resource_id
+  path_part   = "test"
+  depends_on = [ aws_api_gateway_rest_api.MyDemoAPI ]
+}
+
+resource "aws_api_gateway_method" "MyDemoMethod" {
+  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id   = aws_api_gateway_resource.MyDemoResource.id
+  http_method   = "GET"
+  authorization = "NONE"
+  depends_on = [ aws_api_gateway_rest_api.MyDemoAPI ,aws_api_gateway_resource.MyDemoResource ]
+}
+
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id             = aws_api_gateway_resource.MyDemoResource.id
+  http_method             = aws_api_gateway_method.MyDemoMethod.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.test_lambda.invoke_arn
+  depends_on = [ aws_api_gateway_rest_api.MyDemoAPI , aws_api_gateway_method.MyDemoMethod ,aws_api_gateway_resource.MyDemoResource ,aws_lambda_function.test_lambda  ]
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  depends_on = [ aws_api_gateway_rest_api.MyDemoAPI , aws_api_gateway_method.MyDemoMethod ,aws_api_gateway_resource.MyDemoResource, aws_api_gateway_integration.integration, aws_lambda_function.test_lambda   ]
+  #source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
 
